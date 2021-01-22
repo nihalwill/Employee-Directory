9af50b974d5eb690b37409c11cd008b8c614332c
Nihal Williams <nihalwill@gmail.com>
2021-01-21 19:31:24 -0500

    Started adding visual layout files and linked API. Started code to display API values in tables.

---
+++ src/components/EmployeeTable/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css"
+
+const EmployeeTable = (props) => {
+    return (
+      <table className="table table-dark table-sortable text-center">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col" data-field="name" data-sortable="true">
+              <span onClick={() => props.sortBy("name", "last", "first")}>
+                Name
+              </span>
+            </th>
+            <th scope="col">
+              <span onClick={() => props.sortBy("dob", "date")}>DOB</span>
+            </th>
+            <th scope="col">
+              <span onClick={() => props.sortBy("phone")}>Phone</span>
+            </th>
+            <th scope="col">
+              <span onClick={() => props.sortBy("email")}>Email</span>
+            </th>
+            
+          </tr>
+        </thead>
+       
+  
+ 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-center">Employee Directory</h1>
+      <p className="text-center">
+        Click on a column or use the search bar to search for content according to your criteria.
+      </p>
+    </header>
+  );
+};
+
+export default Header;
