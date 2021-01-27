a52c6bcb4e2e09f30128b138b90b7d8ee3974bc9
Nihal Williams <nihalwill@gmail.com>
2021-01-26 19:18:52 -0500

    working on employee table

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 const SearchBar = (props) => {
 
   return (
-    <nav className="navbar navbar-light bg-dark justify-content-center">
+    <nav className="navbar navbar-dark bg-dark justify-content-center">
       <form className="form-inline m-2" onSubmit={props.handleFormSubmit}>
         <input
           className="form-control"
